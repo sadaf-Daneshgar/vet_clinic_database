f8586359c2f943bc12dfd328840eb8b33316a55e
“sadaf <sadaf.batekzai54321@gmial.com>
2023-10-19 21:07:53 +0430

    Create database, table, values and add qures

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
-/* Database schema to keep the structure of entire database. */
+/* Qurey for creating database */
+CREATE Database vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+   id INTEGER NOT NULL UNIQUE PRIMARY KEY ,
+   name VARCHAR(100) NOT NULL,
+   date_of_birth DATE NOT NULL,
+   escape_attempts INT NOT NULL,
+   neutered BOOLEAN NOT NULL,
+   weight_kg DECIMAL(5,2) NOT NULL
 );
