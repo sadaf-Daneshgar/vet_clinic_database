fc3222bd7a07032f334685a42c5e2ecdc17bb806
“sadaf <sadaf.batekzai54321@gmial.com>
2023-10-23 11:38:23 +0430

    delete the day one project queries

--- queries.sql
+++ queries.sql
@@ -1,12 +1,3 @@
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
-SELECT name FROM animals WHERE neutered = True AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = True;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
 /* query and update animals table for second project */
 /* To start a transaction and update the animals specise column and then rollback the changes */
  BEGIN;
--- schema.sql
+++ schema.sql
@@ -1,14 +1,3 @@
-/* Qurey for creating database */
-CREATE Database vet_clinic;
-
-CREATE TABLE animals (
-   id INTEGER NOT NULL UNIQUE PRIMARY KEY ,
-   name VARCHAR(100) NOT NULL,
-   date_of_birth DATE NOT NULL,
-   escape_attempts INTEGER NOT NULL,
-   neutered BOOLEAN NOT NULL,
-   weight_kg DECIMAL(5,2) NOT NULL);
-
 /* Query for adding new column into table */
 /* query and update animals table for second project */
 
