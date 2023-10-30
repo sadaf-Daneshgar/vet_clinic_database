9f4ae30e117fda631ef15231d0bd65b981601473
Ye Min <dhammaarchive@hotmail.com>
2023-10-29 18:40:19 -0700

    Remove the NOT NULL constraint

--- schema.sql
+++ schema.sql
@@ -68,3 +68,6 @@ CREATE TABLE visits (
   
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- Remove the NOT NULL constraint from the age column
+ALTER TABLE owners ALTER COLUMN age DROP NOT NULL;
